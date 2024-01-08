84ae3f830780b53932cef999a250d02c3db5a237
DiliLearngent <2193276981@qq.com>
2024-01-08 15:18:24 +0800

    update JavaAgent

--- JavaAgent/src/main/java/org/dili/AgentTest.java
+++ JavaAgent/src/main/java/org/dili/AgentTest.java
@@ -3,6 +3,8 @@ package org.dili;
 import java.lang.instrument.Instrumentation;
 
 public class AgentTest {
+    // Test Main in JavaBase.src.java.org.dili.javaagent.Main
+    // 此模块打包成jar包即可，在运行上述类时加入-javaagent:jar包即可
     public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println("Premain Start:");
         ClassInfoTransformer transformer = new ClassInfoTransformer();
